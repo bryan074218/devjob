b1e090dafaed7f8b671c815008fbdc57102e221d
bryan074218 <bryangomez665@gmail.com>
2021-08-07 13:41:21 -0600

    renderizando y finzalizando contolador de crear cuenta

--- controllers/usuarioControllers.js
+++ controllers/usuarioControllers.js
@@ -1,9 +1,10 @@
-const mongoose = require('mongoose');
-const Usuarios = require('../models/Usuarios');
 const {
     body,
     validationResult
 } = require('express-validator');
+const mongoose = require('mongoose');
+const Usuarios = require('../models/Usuarios');
+
 
 exports.formCrearCuenta = (req, res)=>{
     res.render('crear-cuenta', {
@@ -11,6 +12,35 @@ exports.formCrearCuenta = (req, res)=>{
         tagLine: 'Comienza a publicar tus vacantes gratis, solo debes crear una cuenta'
     });
 }
+
+exports.validarRegistro = async (req, res, next) => {
+    //sanitizar los campos
+    const rules = [
+        body('nombre').not().isEmpty().withMessage('El nombre es obligatorio').escape(),
+        body('email').isEmail().withMessage('El email es obligatorio').normalizeEmail(),
+        body('password').not().isEmpty().withMessage('El password es obligatorio').escape(),
+        body('confirmar').not().isEmpty().withMessage('Confirmar password es obligatorio').escape(),
+        body('confirmar').equals(req.body.password).withMessage('Los passwords no son iguales')
+    ];
+ 
+    await Promise.all(rules.map(validation => validation.run(req)));
+    const errores = validationResult(req);
+    //si hay errores
+    if (!errores.isEmpty()) {
+        req.flash('error', errores.array().map(error => error.msg));
+        res.render('crear-cuenta', {
+            nombrePagina: 'Crea una cuenta en Devjobs',
+            tagline: 'Comienza a publicar tus vacantes gratis, solo debes crear una cuenta',
+            mensajes: req.flash()
+        })
+        return;
+    }
+ 
+    //si toda la validacion es correcta
+    next();
+}
+ 
+
 
 exports.crearUsuario = async(req, res, next)=>{
     const usuario = new Usuarios(req.body);
@@ -21,34 +51,4 @@ exports.crearUsuario = async(req, res, next)=>{
 
     res.redirect('/iniciar-session');
 
-}
-
-exports.validarRegistro = async(req, res, next)=>{
-        //sanitizar los campos
-
-        const rules = [
-            body('nombre').not().isEmpty().withMessage('El nombre es obligatorio').escape(),
-            body('email').isEmail().withMessage('El email es obligatorio').normalizeEmail(),
-            body('password').not().isEmpty().withMessage('El password es obligatorio').escape(),
-            body('confirmar').not().isEmpty().withMessage('confirmar password es obligatorio').escape(),
-            body('confirmar').equals(req.body.password).withMessage('Los passwords no son iguales')
-        ];
-     
-        await Promise.all(rules.map(validation => validation.run(req)));
-        const errores = validationResult(req);
-        console.log(errores)
-        //si hay errores
-        if (!errores.isEmpty()) {
-            req.flash('error', errores.array().map(error => error.msg));
-            
-            res.render('crear-cuenta', {
-                nombrePagina: 'Crea una cuenta en Devjobs',
-                tagline: 'Comienza a publicar tus vacantes gratis, solo debes crear una cuenta',
-                mensajes: req.flash()
-            })
-            return;
-        }
-     
-        //si toda la validacion es correcta
-        next();
-}
+} 
--- index.js
+++ index.js
@@ -6,20 +6,24 @@ const handlebars = require('handlebars');
 const exphbs = require('express-handlebars');
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
 const path = require('path');
-require('dotenv').config({path: 'variables.env'})
 //importamos las rutasas
 const router = require('./routes');
 const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const bodyParser = require('body-parser');
-const expressValidator = require('express-validator');
-const flash = require('connect-flash');
 
  
+require('dotenv').config({path: 'variables.env'})
+
+
 
  
 const app = express();
+
+
+
 //habilitamos bodyparser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -40,8 +44,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //creamos el puerto donde estara ejecutandose nuestro server
 const port = process.env.PUERTO || 3000;
 
-//iniciamos las rutas
-app.use('/', router());
+
 
 app.use(cookieParser());
 
@@ -53,13 +56,19 @@ app.use(session({
     store: MongoStore.create({mongoUrl: process.env.DATABASE})
 }));
 
-//aletas y flash message
-app.use(flash()); 
-//creamos nuestros middelware
+
+
+
+app.use(flash());
+
+// Crear nuestro middleware
 app.use((req, res, next) => {
     res.locals.mensajes = req.flash();
     next();
 });
+
+//iniciamos las rutas
+app.use('/', router());
 
 // iniciamos el servidor
 app.listen(port,()=>{
