1aab4e9be2eeb122a92e74d2c44107fc30f3d367
bryan074218 <bryangomez665@gmail.com>
2021-08-07 22:13:53 -0600

    finalizando panel de administracion

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,10 +1,12 @@
 const passport = require('passport');
+const mongoose = require('mongoose');
+const Vacante = mongoose.model('vacante');
 
 exports.autenticarUsuario = passport.authenticate('local',{
     successRedirect: '/administracion',
-    failureRedirect: '/iniciar-sesion',
+    failureRedirect: '/iniciar-sesion', 
     failureFlash: true,
-    badRequestMessage: 'Ambos campos son obligatorio'
+    badRequestMessage: 'Ambos campos son obligatorio' 
 });
 
 //revisar si el usuario esta autenticado o no
@@ -18,10 +20,14 @@ exports.verificarUsuario = (req, res, next)=>{
     res.redirect('/iniciar-sesion');
 }
 
-exports.mostrarPanel = (req, res)=>{
+exports.mostrarPanel = async(req, res)=>{
+
+    //consultar el usuario autenticado
+    const vacantes = await Vacante.find({autor: req.user._id});
+
     res.render('administracion',{
         nombrePagina: 'Panel de administracion',
-        tagLine: 'Crea y administras tus vacantes desde aqui'
-    });
+        tagLine: 'Crea y administras tus vacantes desde aqui',
+        vacantes
+    }); 
 }
-
